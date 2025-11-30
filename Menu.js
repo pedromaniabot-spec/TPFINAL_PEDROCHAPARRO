6dfe1e5e81d1c15c06c73337c03bdbbc023f6ee8
pedromaniabot-spec <pedromaniabot@gmail.com>
2025-11-30 20:52:37 -0300

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -1,6 +1,26 @@
 const btnMenu = document.getElementById("btnMenu");
 const menu = document.getElementById("menu");
+const submenus = document.querySelectorAll(".submenu > a"); // Selecciona todos los enlaces de submenú
 
 btnMenu.addEventListener("click", () => {
-    menu.style.display = menu.style.display === "block" ? "none" : "block";
+    menu.classList.toggle("menu-visible");
+});
+
+submenus.forEach(enlace => {
+    enlace.addEventListener("click", (event) => {
+        if (window.innerWidth <= 768) {
+            
+            event.preventDefault(); 
+            
+            const parentLi = enlace.closest(".submenu");
+            
+            parentLi.classList.toggle("activo");
+            
+            document.querySelectorAll(".submenu").forEach(item => {
+                if (item !== parentLi && item.classList.contains("activo")) {
+                    item.classList.remove("activo");
+                }
+            });
+        }
+    });
 });
