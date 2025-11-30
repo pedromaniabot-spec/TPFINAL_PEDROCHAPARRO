81d3779e1ed83ad912e4fd5b1251beae0d4e71cb
pedromaniabot-spec <pedromaniabot@gmail.com>
2025-11-30 10:00:23 -0300

    Update Bienvenida.js

--- Bienvenida.js
+++ Bienvenida.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let nombre = prompt("Ingrese su nombre:");
+    let apellido = prompt("Ingrese su apellido:");
+    let edad = parseInt(prompt("Ingrese su edad:"));
+
+    let mensaje;
+
+    if (edad > 20) {
+        mensaje = "Es mayor de edad.";
+    } else {
+        mensaje = "Es menor de edad.";
+    }
+
+    document.getElementById("mensajeBienvenida").innerHTML =
+        `Bienvenido/a ${nombre} ${apellido}. ${mensaje}`;
+});
 
